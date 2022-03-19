1a2d0a1e4bc1d466b2468699eb21a36c4547ffad
Dodsie <baileydods@gmail.com>
2022-03-19 12:11:55 -0600

    iss

---
+++ index.js
@@ -0,0 +1,43 @@
+const { nextISSTimesForMyLocation } = require('./iss');
+// const fetchCordsByIPoordsByIP = methods.fetchCoordsByIP;
+// const fetchMyIP = methods.fetchMyIP;
+// fetchMyIP((error,ip) => {
+//   if (error) {
+//     console.log("Was not successful", error);
+//     return;
+//   }
+//   console.log("IP address:", ip);
+  
+// });
+
+// fetchCoordsByIP("68.147.173.202", (error,coordinates) => {
+//   if (error) {
+//     console.log('Did not work', null);
+//     return;
+//   }
+//   console.log('worked', coordinates);
+  
+// });
+// const coords = { latitude: '50.9266', longitude: '-113.9726' };
+// fetchISSFlyOverTimes(coords, (error, passTimes) => {
+//   if (error) {
+//     console.log('did not work', error);
+//     return;
+//   }
+//   console.log('worked', passTimes);
+// });
+
+const printPassTimes = (passTimes) => {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+nextISSTimesForMyLocation((error,passTimes) => {
+  if (error) {
+    return console.log('It didnt work!', error);
+  }
+  printPassTimes(passTimes);
+});
